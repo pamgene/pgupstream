7ca753737bbdaccddf596fba7ffafe99d58eb1c0
Rik de Wijn <rdwijn@nl.pamgene.com>
2018-03-20 11:10:28 +0100

    initial

---
+++ R/upstream_analysis.R
@@ -0,0 +1,115 @@
+#' @import pgFCS
+#' @import dplyr
+#' @import plyr
+#' @import ggplot2
+#' @import reshape2
+#' @import foreach
+#' @import doParallel
+#' @import scales
+#' @import data.table
+
+#' @export
+scanAnalysis2g = function(df     ,dbFrame,
+                                dbWeights = c(iviv = 1,PhosphoNET = 1),
+                                scanRank = 4:12,
+                                nPermutations = 500,
+                                minPScore = 300){
+  #run two group.
+  #add dbWeight
+  dbFrame= subset(dbFrame, Database != "phosphoNET" | Kinase_PKinase_PredictorVersion2Score > minPScore)
+  dbFrame = dbFrame %>% group_by(Database) %>% do({
+    data.frame(., dbWeight = dbWeights[[ .$Database[1] ]])
+  })
+
+  ixList = intersectById(dbFrame, df)
+  dbFrame = ixList[[1]]
+  df = ixList[[2]]
+  X = acast(df, colSeq~ID, fun.aggregate = mean, value.var = "value")
+  grp = acast(df, colSeq~ID, value.var = "grp")[,1]
+  grp = as.factor(grp)
+  nCores = detectCores()
+  cl = makeCluster(nCores)
+  registerDoParallel(cl)
+  #print(paste("Please wait, processing on ", nCores, " CPU cores ..."))
+  aPackagesList = c("pgFCS", "reshape2" )
+
+  aScanResult =  foreach(i = scanRank, .packages = aPackagesList) %dopar%
+  {
+    aTop = subset(dbFrame, Kinase_Rank <= i)
+
+    M = acast(aTop, ID ~ Kinase_Name, value.var = "dbWeight", fun.aggregate = max)
+    M[!is.finite(M)] = 0
+    inx2 = intersect(colnames(X), rownames(M))
+    Xi = X[, colnames(X) %in% inx2]
+    M = M[rownames(M) %in% inx2,]
+    # cannot depend on dcast for correct order
+    M = M[order(rownames(M)),]
+    Xi = Xi[, order(colnames(Xi))]
+    if(!all(rownames(M)== colnames(Xi))) stop("Mismatch between data matrix and upstream kinase matrix")
+    aResult = fcs(Xi, M, grp, nPerms = nPermutations)
+    aResult = aResult[order(aResult$combinedScore, decreasing = TRUE),]
+    return(data.table(mxRank = i, aResult = list(aResult), X = list(Xi), M = list(M)))
+  }
+  stopCluster(cl)
+  return(aScanResult)
+}
+
+#'@export
+scanAnalysis0 = function(df     ,dbFrame,
+                          dbWeights = c(HPRD = 1,PhosphoNET = 1,Phosphosite = 1,Reactome = 1),
+                          scanRank = 4:12,
+                          nPermutations = 500,
+                          minPScore = 300){
+
+  # run a sinlge column, without grouping
+  #add dbWeight
+  dbFrame= subset(dbFrame, Database != "phosphoNET" |  Kinase_PKinase_PredictorVersion2Score > minPScore)
+  dbFrame = dbFrame %>% group_by(Database) %>% do({
+    data.frame(., dbWeight = dbWeights[[ .$Database[1] ]])
+  })
+
+  ixList = intersectById(dbFrame, df)
+  dbFrame = ixList[[1]]
+  df = ixList[[2]]
+
+  nCores = detectCores()
+  cl = makeCluster(nCores)
+  registerDoParallel(cl)
+  #print(paste("Please wait, processing on ", nCores, " CPU cores ..."))
+  aPackagesList = c("pgFCS", "reshape2" )
+
+  aScanResult =  foreach(i = scanRank, .packages = aPackagesList) %dopar%
+  #aScanResult = for(i in scanRank)#debug
+  {
+    aTop = subset(dbFrame, Kinase_Rank <= i)
+    M = acast(aTop, ID ~ Kinase_Name, value.var = "dbWeight", fun.aggregate = max)
+    M[!is.finite(M)] = 0
+    inx2 = intersect(df$ID, rownames(M))
+    dfx =  df%>%filter(ID %in% inx2)
+    X = matrix(ncol = dim(dfx)[1], nrow = 1, data = dfx$value)
+    colnames(X) = dfx$ID
+    M = M[rownames(M) %in% inx2,]
+    M = M[order(rownames(M)),]
+    X = X[, order(colnames(X))]
+    if(!all(rownames(M)== colnames(X))) stop("Mismatch between data matrix and upstream kinase matrix")
+    aResult = fcs(X, M, statFun = stat.identity, phenoGrp = NULL, phenoPerms = FALSE, nPerms = nPermutations)
+    aResult = aResult[order(aResult$combinedScore, decreasing = TRUE),]
+    return(data.table(mxRank = i, aResult = list(aResult), X = list(X), M = list(M)))
+  }
+  stopCluster(cl)
+  return(aScanResult)
+}
+
+
+intersectById = function(df1, df2){
+  df1$ID = droplevels(df1$ID)
+  df2$ID = droplevels(df2$ID)
+  isct = intersect(df1$ID, df2$ID)
+  df1 = subset(df1, ID %in% isct)
+  df2 = subset(df2, ID %in% isct)
+  df1$ID = droplevels(df1$ID)
+  df2$ID = droplevels(df2$ID)
+  return(list(df1, df2))
+}
+
+
---
+++ R/upstream_output.R
@@ -0,0 +1,161 @@
+rescale = function (x, to = c(0, 1), from = range(x, na.rm = TRUE, finite = TRUE), clip = FALSE)
+{
+  if (zero_range(from) || zero_range(to)) {
+    return(ifelse(is.na(x), NA, mean(to)))
+  }
+  x = (x - from[1])/diff(from) * diff(to) + to[1]
+
+  if(clip){
+    # elegant clipping by Josh O'Brien @ Stackoverflow
+    #http://stackoverflow.com/questions/13868963/clip-values-between-a-minimum-and-maximum-allowed-value-in-r
+    x = to[1] + (x > to[1])*(x-to[1])  - (x > to[2]) * (x - to[2])
+  }
+  return(x)
+}
+#' @export
+scorePlot = function(aResult, plotorder = "score"){
+  aFull = ldply(aResult, .fun = function(.)return(data.frame(.$aResult, mxRank = .$mxRank) ))
+  sp = makeScorePlot(aFull, plotorder)
+}
+
+#' @export
+makeScorePlot = function (aFrame, plotorder = "score"){
+
+  aFrame = aFrame %>% group_by(ClassName) %>% mutate(meanScore = mean(combinedScore, na.rm = TRUE), meanStat = mean(NormalizedSetStat, na.rm = TRUE))
+  aFrame = aFrame %>% group_by(mxRank) %>% mutate(ClassRank = rank(-combinedScore))
+
+  if (plotorder == "score"){
+    orderx = aFrame$combinedScore
+  }
+
+  if (plotorder == "stat"){
+    orderx = -aFrame$NormalizedSetStat
+  }
+
+  prt2 = ggplot(aFrame, aes(x= reorder(ClassName, orderx, median), y = NormalizedSetStat)) + geom_boxplot()
+  prt2 = prt2 + geom_point(aes(colour = rescale(pFeatureScore, from = c(0,2), clip = TRUE), size = nFeatures))
+  prt2 = prt2 + geom_abline(slope = 0)
+  prt2 = prt2 + coord_flip() + theme_bw()
+  prt2 = prt2 + scale_colour_gradientn(name = "Specificity Score",space = "rgb",
+                                       colours = c("black", "white", "red"),
+                                       values = c(0,1.3, 2)/2,
+                                       breaks = c(0,1.3, 2)/2,
+                                       labels = c(0, 1.3, 2),
+                                       limits = c(0,1))
+  prt2 = prt2 + ylab("Normalized kinase statistic") + xlab("Kinase name")
+  yRange = layer_scales(prt2)$y$range$range
+  prt2 = prt2 + scale_y_continuous(position = "top", limits = c( min(0, yRange[1]), max(0, yRange[2])) )
+  prt2 = prt2 + guides( size = guide_legend("Number of peptides included"), color = guide_colorbar("Specificity score"))
+  return(prt2)
+}
+
+#' @export
+makeVolcanoPlot = function(aSummary){
+  vp = ggplot(aSummary, aes(
+                              x = meanStat,
+                              y = medianScore,
+                              colour = rescale(meanFeatScore, from = c(0,2), clip = TRUE),
+                              label = ClassName,
+                              size = meanSetSize,
+                              alpha =  1-rescale(sdStat, from = c(0,1), clip = TRUE )
+                            )
+              )
+
+  vp = vp + geom_text() + scale_colour_gradientn(name = "Specificity Score",space = "rgb",
+                                   colours = c("black", "white", "red"),
+                                   values = c(0,1.3, 2)/2,
+                                   breaks = c(0,1.3, 2)/2,
+                                   labels = c(0, 1.3, 2),
+                                   limits = c(0,1))
+
+  vp = vp + scale_alpha("Consistency", limits = c(0,1))
+  vp = vp + scale_size("Number of peptides")
+  vp = vp + theme_bw() + theme(panel.background = element_rect(fill = 'lightyellow', colour = 'black'))
+  return(vp)
+}
+
+ubMelt = function(aList){
+  aMelt = melt(aList$M)
+  aMelt = subset(aMelt, value > 0)
+  aMelt$value = aList$mxRank
+  aMelt = aMelt[,1:3]
+  colnames(aMelt) = c("Peptide.ID", "kinase.name", "mxrank")
+  return(aMelt)
+}
+
+#' @export
+makePerPeptidePlot = function(df, dbFrame, scanRank, minPScore){
+  dbFrame= subset(dbFrame, Database != "phosphoNET" |  Kinase_PKinase_PredictorVersion2Score > minPScore)
+  dbFrame = subset(dbFrame, Kinase_Rank <= scanRank)
+  ixList = intersectById(dbFrame, df)
+  dbFrame = ixList[[1]]
+  df = ixList[[2]]
+  dbFrame = dbFrame %>% group_by(ID) %>% dplyr::summarise(lowestRank = min(Kinase_Rank))
+  if (!is.null(df[["grp"]])){
+      perPepStats = df %>% group_by(ID) %>% do({
+      thisPep = subset(dbFrame, ID == .$ID[1])
+      aTest = t.test(value ~ grp, data = ., var.equal = TRUE)
+      aResult = data.frame(pes = diff(aTest$estimate),
+                           ciu = -aTest$conf.int[1],
+                           cil = -aTest$conf.int[2],
+                           lowRank = thisPep$lowestRank
+      )
+    })
+    ppp = ggplot(perPepStats, aes(x = reorder(ID, -lowRank), colour = as.factor(lowRank), y = pes, ymin = cil, ymax = ciu)) + geom_point() + geom_errorbar()
+    ppp = ppp + coord_flip()
+    ppp = ppp + xlab("Peptide ID") + ylab("Group difference")
+    ppp = ppp + geom_abline(slope = 0)
+    ppp = ppp + guides( color = guide_legend("Lowest Kinase Rank"))
+
+  } else {
+    perPepStats = df %>% group_by(ID) %>% do({
+      thisPep = subset(dbFrame, ID == .$ID[1])
+      aResult = data.frame(value = .$value,
+                           lowRank = thisPep$lowestRank
+      )
+    })
+    ppp = ggplot(perPepStats, aes(x = reorder(ID, -lowRank), colour = as.factor(lowRank), y = value)) + geom_point()
+    ppp = ppp + coord_flip()
+    ppp = ppp + xlab("Peptide ID") + ylab("Value")
+    ppp = ppp + geom_abline(slope = 0)
+    ppp = ppp + guides( color = guide_legend("Lowest Kinase Rank"))
+  }
+  yRange = layer_scales(ppp)$y$range$range
+
+  ppp = ppp + scale_y_continuous(position = "top", limits = c(-max(abs(yRange)), max(abs(yRange))))
+  return(ppp)
+}
+
+#' @export
+makeDetailsTable = function(df, dbFrame, scanRank, minPScore){
+  dbFrame= subset(dbFrame, Database != "phosphoNET" |  Kinase_PKinase_PredictorVersion2Score > minPScore)
+  dbFrame = subset(dbFrame, Kinase_Rank <= scanRank)
+  ixList = intersectById(dbFrame, df)
+  dbFrame = ixList[[1]]
+  outFrame = dbFrame %>% group_by(ID, PepProtein_PhosLink, PepProtein_UniprotName) %>%
+    dplyr::summarise(Database = paste(Database, collapse = " / "), Kinase_Rank = min(Kinase_Rank) )
+  outFrame = outFrame %>% arrange(Kinase_Rank, -as.integer(ID))
+  return(outFrame)
+}
+
+#' @export
+makeSummary = function(df){
+  aSum = df %>% group_by(ClassName) %>% dplyr::summarise(meanFeatScore = mean(pFeatureScore, na.rm = TRUE),
+                                                           meanPhenoScore = mean(pPhenoScore, na.rm = TRUE),
+                                                           meanScore = mean(combinedScore, na.rm = TRUE),
+                                                           medianScore = median(combinedScore, na.rm = TRUE),
+                                                           meanStat = mean(NormalizedSetStat, na.rm = TRUE),
+                                                           medianStat = median(NormalizedSetStat, na.rm = TRUE),
+                                                           sdStat   = sd(NormalizedSetStat, na.rm = TRUE),
+                                                           meanSetSize =mean(nFeatures, na.rm = TRUE))
+  aSum = aSum%>% arrange(-medianScore)
+  return(aSum)
+}
+
+#' @export
+getSettingsInfo = function(settings){
+  return(settings)
+}
+
+
+
